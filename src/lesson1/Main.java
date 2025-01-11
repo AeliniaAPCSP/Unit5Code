6259ed0f99581f7efa75e74f16e361d74228ea04
AeliniaAPCSP <enquista@wlhs.wlwv.k12.or.us>
2025-01-10 17:23:56 -0800

    Update Main.java

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -2,12 +2,17 @@ package lesson1;
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount myAccount = new BankAccount(50);
-        System.out.println(myAccount.getBalance());
+        BankAccount myAccount = new BankAccount("John", 50);
+        System.out.println(myAccount.toString());
         myAccount.deposit(10);
-        System.out.println(myAccount.getBalance());
+        System.out.println(myAccount.toString());
         myAccount.withdraw(30);
-        System.out.println(myAccount.getBalance());
-
+        System.out.println(myAccount.toString());
+        myAccount.changeInterest(.04);
+        myAccount.addInterest();
+        System.out.println(myAccount.toString());
+        System.out.println(myAccount.getAccountNumber());
+        myAccount.changeOwner("John 2.0");
+        System.out.println(myAccount.getOwner());
     }
 }
